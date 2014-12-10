b0f225eabc9b6d9c7a48516e3fee48bf164e7802
Garrett Bryan <garrettdavisbryan@gmail.com>
2014-12-09 22:32:45 -0500

    update acutal bio data.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,4 @@
 
-
 var bio = {
   "name": "Garrett Bryan",
   "role": "Web Developer",
@@ -8,12 +7,87 @@ var bio = {
     "email":"garrettdavisbryan@gmail.com",
     "github":"https://github.com/garrettbryan",
     "twitter":"@GarrettDBryan",
-    "location":"Raleigh"
+    "location":"Raleigh, NC"
   },
   "bioPic": "images/garrettrockclimbing300x300.jpg",
   "welcomeMessage": "Hi, nice to meet you.",
   "skills": ["surfing", "snowboarding", "etc"]
 };
+
+var education = {
+  "schools": [
+    {
+      "name": "UCLA",
+      "location": "Los Angeles",
+      "degree": "drop-out",
+      "majors": ["Material Science"],
+      "dates": "Fall 2010 - Spring 2011",
+      "url": "ucla.edu"
+    },
+    {
+      "name": "Los Angeles City College",
+      "location": "Los Angeles",
+      "degree": "Transfer Student",
+      "majors": ["Material Science"],
+      "dates": "omg",
+      "url": "lacitycollege.edu"
+    },
+    {
+      "name": "Los Angeles Valley College",
+      "location": "Valley Glen",
+      "degree": "Transfer Student",
+      "majors": ["Material Science", "Cinema"],
+      "dates": "omg",
+      "url": "lavc.edu"
+    }
+  ],
+  "onlineCourses": [
+    {
+    "title": "Front-End Developer Nanodegree",
+    "school": "Udacity",
+    "dates": "December Cohort",
+    "url": "https://www.udacity.com/course/nd001"
+    }
+  ]
+};
+
+var work = {
+  "jobs": [
+    {
+      "employer": "Fotokem",
+      "title": "Digital IO Operator",
+      "location": "Burbank, CA",
+      "dates": "July 2006 to November 2013",
+      "description": "Writing a lot of perl scripts."
+    },
+    {
+      "employer": "Fotokem",
+      "title": "Weekend Film Dailies Scheduler",
+      "location": "Burbank, CA",
+      "dates": "June 2005 to July 2006",
+      "description": "Schedule Film Processing"
+    },
+    {
+      "employer": "Los Angeles Valley College",
+      "title": "Math Tutor",
+      "location": "Valley Glen",
+      "dates": "2007",
+      "description": ""
+    }
+  ]
+};
+
+var projects = {
+  "projects": [
+    {
+      "title": "my life",
+      "dates": "June 27, 1980 - sometime in the future",
+      "description": "A rock and a hardplace, mostly my own doing.",
+      "images": []
+    }
+  ]
+};
+
 
 var formattedName = HTMLheaderName.replace("%data%", bio.name);
 var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
@@ -37,38 +111,7 @@ var fPosition =
 $("#main").append(work["position"]);
 
 
-var education = {
-  "schools": [
-    {
-      "name": "UCLA",
-      "city": "Los Angeles",
-      "major": "Material Science",
-      "dates": "Fall 2010 - Spring 2011"
-    },
-    {
-      "name": "Los Angeles City College",
-      "city": "Los Angeles",
-      "major": "Transfer Student",
-      "dates": "omg",
-      "courses": ["Differential Equations", "Multivariable Calculus", "Linear Algebra", "Chemistry 102"]
-    },
-    {
-      "name": "Los Angeles Valley College",
-      "city": "Valley Glen",
-      "major": "Transfer Student",
-      "dates": "omg",
-      "courses": ["Integral Calculus", "Differential Calculus", "Chemistry 101"]
-    }
-  ],
-  "onlineCourses": [
-    {
-    "title": "Front-End Developer Nanodegree",
-    "school": "Udacity",
-    "dates": "December Cohort",
-    "url": "https://www.udacity.com/course/nd001"
-    }
-  ]
-};
+
 ed["name"] = "UCLA";
 ed["years"] = [2010,2011];
 ed["city"] = "Los Angeles";
